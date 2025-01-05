6f25f051b11be521e701d9244887900fd52dff0e
davetrave <daveamwh@gmail.com>
2025-01-05 15:53:43 +0300

    Changed Color Themes

--- src/components/HintVideoCard.jsx
+++ src/components/HintVideoCard.jsx
@@ -14,6 +14,7 @@ const HintVideoCard = () => {
       className="w-full h-24 bg-gradient-to-r from-purple-500 via-blue-500 to-purple-500 animate-gradient-x cursor-pointer flex items-center justify-center"
     >
       <h3 className="text-white text-sm font-bold">Click Here to Watch how to use the App</h3>
+      <div className="badge absolute top-2 right-2"></div>
     </div>
   );
 };
